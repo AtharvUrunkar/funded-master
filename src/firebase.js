1896765f146fd35f449addfe4fe2fe0304157659
AtharvUrunkar <atharvurunkar@gmail.com>
2026-05-04 16:49:44 +0530

    craeted .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyA6V-E4N0qAnMsMcoVBh2U-q_NFKjV-0Qk",
-  authDomain: "funded-master-a8354.firebaseapp.com",
-  projectId: "funded-master-a8354",
-  storageBucket: "funded-master-a8354.firebasestorage.app",
-  messagingSenderId: "432362657414",
-  appId: "1:432362657414:web:e19a2682d5f5269ab2a307"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
